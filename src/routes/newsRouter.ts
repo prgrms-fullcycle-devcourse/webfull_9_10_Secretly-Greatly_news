7f89285ffceff35518f2da42a8de0c67d0eeb5b3
leekyuhyun <leekh010502@gmail.com>
2026-06-07 13:57:21 +0900

    deploy : 빌드 에러 수정

--- src/routes/newsRouter.ts
+++ src/routes/newsRouter.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { getLatestNews } from '../controllers/newsController.js';
 
-const router = Router();
+const router: Router = Router();
 
 // GET /api/news/latest
 router.get('/latest', getLatestNews);
